6c2306971791219c5f03432d39f438126bb0a218
andrei <rezmerita.andrey@gmail.com>
2020-11-09 20:59:21 +0100

    deploying to heroku

---
+++ server.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+app.use(express.static(_dirname + '/dist/practica-andrei-razmerita'));
+app.get('/*', function (req, res) {
+    res.sendFile(path.join(_dirname + '/dist/practica-andrei-razmerita/index.html'))
+});
+app.listen(process.env.PORT || 8080);
