99ad9ad0d24e00c7eb3a550b60d9884a8def1efe
andrei <rezmerita.andrey@gmail.com>
2020-11-09 21:45:05 +0100

    nice4

--- server.js
+++ server.js
@@ -3,6 +3,5 @@ const path = require('path');
 const app = express();
 app.use(express.static(_dirname + '/dist/practica-andrei-razmerita'));
 app.get('/*', function (req, res) {
-    res.sendFile(path.join(_dirname + '/dist/practica-andrei-razmerita/index.html'))
-});
+    res.sendFile(path.join(_dirname + '/dist/practica-andrei-razmerita/index.html'));});
 app.listen(process.env.PORT || 8080);
