29625f3e4d0aa750fa29da6a50d6e95215579eb5
andrei <rezmerita.andrey@gmail.com>
2020-11-09 22:08:20 +0100

    message

--- server.js
+++ server.js
@@ -4,4 +4,4 @@ const app = express();
 app.use(express.static(_dirname + '/dist/practica-andrei-razmerita'));
 app.get('/*', function (req, res) {
     res.sendFile(path.join(_dirname + '/dist/practica-andrei-razmerita/index.html'));});
-app.listen(process.env.PORT || 8080);
+app.listen(process.env.PORT || 8080);heroku logs --tail
