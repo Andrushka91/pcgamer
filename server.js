3f49738fcd4178b82a6b72170e6582c27231829d
andrei <rezmerita.andrey@gmail.com>
2020-11-10 09:25:48 +0100

    swich server settings.js

--- server.js
+++ server.js
@@ -4,6 +4,6 @@ const app = express();
 app.use(express.static(_dirname + '/dist/practica-andrei-razmerita'));
 app.get('/*', function (req, res) {
     res.sendFile(path.join(_dirname + '/dist/practica-andrei-razmerita/index.html'));});
-    pp.listen(process.env.PORT || 8080, function(){
+    pp.listen(process.env.PORT || 3000, function(){
         console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
       });
