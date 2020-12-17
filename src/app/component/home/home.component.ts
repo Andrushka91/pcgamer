333dc7f5b6c2785742ea39fa9bf8a731bc4ff2cd
Andrushka91 <rezmerita.andrey@gmail.com>
2020-12-17 22:07:50 +0100

    First try

--- src/app/component/home/home.component.ts
+++ src/app/component/home/home.component.ts
@@ -4,12 +4,16 @@ import { TranslateService } from '@ngx-translate/core';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css'],
 })
 export class HomeComponent {
-
   constructor(private translateService: TranslateService) {
-    this.translateService.use(localStorage.getItem('language'));
+
+    if (localStorage.getItem('language') != null) {
+      this.translateService.use(localStorage.getItem('language'));
+
+    } else {
+      this.translateService.use('en');
+    }
   }
- 
 }
