9714f1b4ef73ecc80bd482b9198ada5d471d9deb
Andrushka91 <rezmerita.andrey@gmail.com>
2020-12-18 06:41:46 +0100

    4th attempt to deploay

--- server.js
+++ server.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-app.use(express.static(_dirname + '/dist/practica-andrei-razmerita'));
+
+app.use(express.static(_dirname + '/dist/angular-pc-gamer-shop'));
 app.get('/*', function (req, res) {
-    res.sendFile(path.join(_dirname + '/dist/practica-andrei-razmerita/index.html'));});
+    res.sendFile(path.join(_dirname + '/dist/angular-pc-gamer-shop/index.html'));});
     pp.listen(process.env.PORT || 3000, function(){
         console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
       });
