3cf9055d25e012bb80296cbef0e6e950c38767e2
Andrushka91 <rezmerita.andrey@gmail.com>
2020-12-18 13:12:50 +0100

    lol

--- server.js
+++ server.js
@@ -5,6 +5,5 @@ const app = express();
 app.use(express.static(_dirname + '/dist/angular-pc-gamer-shop'));
 app.get('/*', function (req, res) {
     res.sendFile(path.join(_dirname + '/dist/angular-pc-gamer-shop/index.html'));});
-    pp.listen(process.env.PORT || 3000, function(){
-        console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
-      });
+
+    pp.listen(process.env.PORT || 3000);
